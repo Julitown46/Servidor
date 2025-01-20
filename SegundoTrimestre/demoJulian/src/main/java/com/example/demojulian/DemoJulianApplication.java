dece9f0756664145334d342b4876615cc9e4d499
Julitown46 <morenojulian4502@gmail.com>
2025-01-20 12:01:28 +0100

    empezamos spring

---
+++ SegundoTrimestre/demoJulian/src/main/java/com/example/demojulian/DemoJulianApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojulian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJulianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJulianApplication.class, args);
+    }
+
+}
---
+++ SegundoTrimestre/demoJulian/src/test/java/com/example/demojulian/DemoJulianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojulian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJulianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
